2f0be508eff04c0f0d071b5521d9e121214184ef
d4mn13l <131522659+d4mn13l@users.noreply.github.com>
2023-04-23 12:34:06 +0000

    python app.py starts the server

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from flask_login import (
     login_required,
 )
 import json
-from openpyxl import load_workbook
 import time
 import datetime
 
@@ -85,3 +84,6 @@ def create_log(done):
     date = datetime.datetime.now()
     if done == True:
         return date.strftime("%c"), ", alle Aufgaben erledigt."
+
+if __name__ == "__main__":
+    app.run(debug=True)
